36211e7b0d83af85787846d6f41b4f2d731eb686
Ricardo Rodriguez <ricardo.rodriguez4@yahoo.com>
2020-06-28 21:53:10 -0500

    users with templates

---
+++ user_app/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render, redirect, HttpResponse
+from .models import User
+
+# Create your views here.
+def index(request):
+    context = {
+        'users' : User.objects.all()
+    }
+    return render(request,'index.html',context)
+
+def create(request):
+    User.objects.create(
+        first_name = request.POST['first_name'],
+        last_name = request.POST['last_name'],
+        email_address = request.POST['email_address'],
+        age = request.POST['age']
+    )
+    return redirect('/')
